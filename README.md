0cb05bfca4fa461a6ecc271d85664d4b46d6800a
Clive Walkden <clivewalkden@gmail.com>
2012-01-16 15:05:59 +0000

    Added the default value so when value is empty the script still works

---
+++ README.md
@@ -0,0 +1,15 @@
+##cwTripleState
+
+## Changelog:
+
+### Version 0.3 - January 16 2012
+
+* added the default value so when value is empty the script still works
+
+### Version 0.2 - November 28 2011
+
+* added the return false and unbind click to stop multiple firings
+
+### Version 0.1 - November 25 2011
+
+* initial release
--- jquery.triplestate-0.3.js
+++ jquery.triplestate-0.3.js
@@ -3,18 +3,16 @@ cwTripleState
 
 Developed By: Clive Walkden (http://www.sozodesign.co.uk)
 Inspired By: All the non user friendly, overcomplicated triple state solutions ;)
-Version: 0.2
+Version: 0.3
 
 Copyright: Feel free to redistribute the script/modify it, as
                    long as you leave my infos at the top.
 
 ChangeLog:
+16/01/2012 - v0.3 - added the default value so when value is empty the script still works
 28/11/2011 - v0.2 - added the return false and unbind click to stop multiple firings
 25/11/2011 - v0.1 - initial release
 ----------------------------------------------------------------------------- */
-/**
- * @TODO if value in input is empty script fails
- */
 
 (function( $ ){
 
@@ -62,18 +60,18 @@ ChangeLog:
                 //console.log("input id: "+input.attr('id')+" state: "+state);
 
                 switch(state) {
-                    case 0:
-                        input.val('2');
-                        obj.removeClass(settings.statedef).removeClass(settings.state1).removeClass(settings.state2).addClass(settings.state2);
+                    case 2:
+                        input.val('1');
+                        obj.removeClass(settings.statedef).removeClass(settings.state1).removeClass(settings.state2).addClass(settings.state1);
                         break;
                     case 1:
                         input.val('0');
                         obj.removeClass(settings.statedef).removeClass(settings.state1).removeClass(settings.state2).addClass(settings.statedef);
                         break;
-                    case 2:
-                        input.val('1');
-                        obj.removeClass(settings.statedef).removeClass(settings.state1).removeClass(settings.state2).addClass(settings.state1);
-                        break;
+                    case 0:
+                    default:
+                        input.val('2');
+                        obj.removeClass(settings.statedef).removeClass(settings.state1).removeClass(settings.state2).addClass(settings.state2);
                 }
                 //console.log("new state: "+input.val());
                 return false;
